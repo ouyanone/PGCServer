d4cd4586fef112b8d667b849f821013c9709c044
ouyan <ouyan@yans-mbp.fios-router.home>
2026-05-08 08:52:14 -0400

    fix cognitio issue

--- src/main/java/com/shiyuan/config/SecurityConfiguration.java
+++ src/main/java/com/shiyuan/config/SecurityConfiguration.java
@@ -1,18 +1,26 @@
 package com.shiyuan.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(Customizer.withDefaults())
+		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+		.csrf(Customizer.withDefaults())
 				.authorizeHttpRequests(
 						authz -> authz.requestMatchers("/webapi/admin/**").authenticated()
 									  .anyRequest().permitAll())
@@ -24,5 +32,18 @@ public class SecurityConfiguration {
 				.csrf(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
+	
+	
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://us-east-1xdeoxg5ea.auth.us-east-1.amazoncognito.com"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
 }
